12750f63948f00c06e8fc05abe2d30f984c094f8
Devendra Shukla <devshukla0123@gmail.com>
2024-05-24 12:36:54 +0530

    commit install package code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';//for router-com v6
 import Home from './components/Home';
 import Shop from './components/Shop';
 import ViewCart from './components/ViewCart';
+import LoadingBar from 'react-top-loading-bar';
 
 const App = () => {
-
+  const [progess, setProgress] = useState(100);
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <LoadingBar
+          color='#f11946'
+          progress={progess}
+        />
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/shop" element={<Shop />}></Route>
